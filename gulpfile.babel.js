e40e63e553bbece945027324b7ebf397507047f0
AprilOrange <aprilorange.net@icloud.com>
2015-09-09 22:12:06 +0800

    arcive

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,30 +1,37 @@
 import gulp from 'gulp'
-import webpack from 'webpack'
-import webpackConfig from './webpack.config'
+import babel from 'gulp-babel'
 import gutil from 'gulp-util'
 import stylus from 'gulp-stylus'
 
-gulp.task('webpack', (cb) => {
-  let devWebpackConfig = Object.create(webpackConfig)
-  devWebpackConfig.watch = true
-  webpack(devWebpackConfig, (err, stats) => {
-    gutil.log('[webpack:build]', stats.toString({
-      colors: true
-    }))
-    cb()
-  })
+const paths = {
+  babel: {
+    src: ['./src/js/*.*', './src/js/*/*.*'],
+    target: './lib/js'
+  },
+  stylus: {
+    main: './src/styl/main.styl',
+    src: './src/styl/*.styl',
+    target: './lib/css'
+  }
+}
+
+gulp.task('babel', () => {
+  gulp.src(paths.babel.src)
+    .pipe(babel())
+    .pipe(gulp.dest(paths.babel.target))
 })
 
 gulp.task('stylus', () => {
-  gulp.src('./src/styl/main.styl')
+  gulp.src(paths.stylus.main)
     .pipe(stylus())
-    .pipe(gulp.dest('./lib/css'))
+    .pipe(gulp.dest(paths.stylus.target))
 })
 
 gulp.task('watch', () => {
-  gulp.watch('./src/styl/*.styl', ['stylus'])
+  gulp.watch(paths.babel.src, ['babel'])
+  gulp.watch(paths.stylus.src, ['stylus'])
 })
 
-gulp.task('build', ['webpack', 'stylus'])
+gulp.task('build', ['babel', 'stylus'])
 
 gulp.task('default', ['build', 'watch'])
--- src/js/chelly.js
+++ src/js/chelly.js
@@ -1,18 +1,51 @@
 import remote from 'remote'
 import React from 'react'
 import Router, {Location, Locations, Link, NotFound} from 'react-router-component'
+import localdb from 'localdb'
 
 // set Menu
-import menuTemplate from './menuTemplate'
+import menuTemplate from './lib/js/menuTemplate'
 let Menu = remote.require('menu')
 const menu = Menu.buildFromTemplate(menuTemplate)
 Menu.setApplicationMenu(menu)
 
+// config localStorage database
+let Config = new localdb('Config', 'Object')
+Config.set('init', true)
+console.log(Config.get())
+
+class ChooseFolder extends React.Component {
+  componentDidMount () {
+    const input = React.findDOMNode(this.refs.input)
+    const folder = React.findDOMNode(this.refs.folder)
+
+    input.setAttribute('webkitdirectory', '')
+    const marginTop = - ( folder.clientHeight / 2 )
+    folder.style.marginTop = marginTop + 'px'
+  }
+  openChooser () {
+    const input = React.findDOMNode(this.refs.input)
+    input.click()
+  }
+  changeFolder (e) {
+    Config.set('folder', e.target.value)
+  }
+  render () {
+    return (
+      <div className="choose-folder" ref="folder" onClick={this.openChooser.bind(this)}>
+        现在就选择一个目录存储你的笔记！
+        <br />
+        <div className="choose-icon"><span className="octicon octicon-plus"></span></div>
+        <input style={{display:'none'}} ref="input" type="file" onChange={this.changeFolder.bind(this)} />
+      </div>
+    )
+  }
+}
 
 class Home extends React.Component {
   render () {
-    if (false) {
-      return <div className="emptyState">Empty</div>
+    if (!Config.get().folder) {
+      return <ChooseFolder />
     }
     return <div>
       <Sidebar/>
